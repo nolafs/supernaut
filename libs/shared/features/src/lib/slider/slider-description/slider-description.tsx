708500a2ec6132b52b5aa7bb093dd5b8bf023dd3
Olaf Siebert <olaf@biffnewmedia.com>
2024-05-02 16:47:58 +0200

    feat: slider nav

--- libs/shared/features/src/lib/slider/slider-description/slider-description.tsx
+++ libs/shared/features/src/lib/slider/slider-description/slider-description.tsx
@@ -14,22 +14,21 @@ export interface SliderDescriptionProps {
 export function SliderDescription({id, title, description}: SliderDescriptionProps) {
 
   const main = useRef<HTMLDivElement | any>();
-
+  const split = useRef<SplitText | any>();
   console.log('id', id)
 
 
   useGSAP(() => {
+    split.current = new SplitText('.header', {type: 'chars'})
 
-    const split = new SplitText('.header', {type: 'chars'})
-
-   gsap.from([split.chars, '.body' ], {opacity: 0, y:"100%", duration:0.5, stagger: 0.05, ease: 'power3.inOut'});
+    gsap.from([split.current.chars, '.body' ], {opacity: 0, y:"100%", duration:0.5, stagger: 0.05, ease: 'power3.inOut'});
   }, {dependencies: [id, title], scope: main})
 
 
 
   return (
     <div ref={main} className={'absolute text-primary bottom-0 mx-6 md:mx-12 mb-6 md:mb-14 flex flex-col md:space-y-7 z-20'}>
-      <h2 className={'header text-3xl md:text-[48px] font-medium tracking-tighter overflow-hidden'}>{title}</h2>
+      <h2 className={'header text-3xl md:text-[48px] font-medium tracking-tighter overflow-hidden p-1'}>{title}</h2>
       <p className={'body text-2xl lg:text-3xl font-medium hidden md:block'}>{description}</p>
     </div>
   );
