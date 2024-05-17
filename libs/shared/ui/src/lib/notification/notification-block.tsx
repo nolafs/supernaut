0134a86b63dd74c8478c70559c3dce8279c76c38
Olaf Siebert <olaf@biffnewmedia.com>
2024-05-17 18:00:24 +0200

    feat:  image component

--- libs/shared/ui/src/lib/notification/notification-block.tsx
+++ libs/shared/ui/src/lib/notification/notification-block.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable-next-line */
+import {ExclamationCircleIcon, ExclamationTriangleIcon, InformationCircleIcon} from '@heroicons/react/16/solid';
+import {CheckCircleIcon} from '@contentful/f36-icons';
+
 export interface NotificationBlockProps {
   body: string;
   type: 'info' | 'warning' | 'error' | 'success';
@@ -8,13 +11,21 @@ export function NotificationBlock({body, type}: NotificationBlockProps) {
   return (
     <div className={'flex w-full justify-center items-center'}>
       <div className={
-        `p-5 text-white text-center ${
+        `p-5 text-white text-center flex flex-row space-x-2.5 ${
           type === 'info' && 'bg-blue-500'
           || type === 'warning' && 'bg-yellow-500'
           || type === 'error' && 'bg-red-500'
           || type === 'success' && 'bg-green-500'
         }`
-      }>{body}</div>
+      }>
+        <span>
+          {type === 'info' && <InformationCircleIcon className={'w-6 h-6'}/>}
+          {type === 'warning' && <ExclamationCircleIcon className={'w-6 h-6'}/>}
+          {type === 'error' && <ExclamationTriangleIcon className={'w-6 h-6'}/>}
+          {type === 'success' && <CheckCircleIcon className={'w-6 h-6'}/>}
+        </span>
+        <span>{body}</span>
+      </div>
     </div>
   );
 }
