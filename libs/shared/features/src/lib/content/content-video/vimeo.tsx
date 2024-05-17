b23edc50b88dba10a24e934a2666918d7595075a
Olaf Siebert <olaf@biffnewmedia.com>
2024-05-17 17:16:28 +0200

    feat:  image component

--- libs/shared/features/src/lib/content/content-video/vimeo.tsx
+++ libs/shared/features/src/lib/content/content-video/vimeo.tsx
@@ -37,8 +37,16 @@ export function Vimeo({id, src, title, poster, width = 1920, height = 1200}: Vim
     setShowPlayer(true);
   }
 
+  const handlePause = () => {
+    setShowPlayer(true);
+  }
 
-  return (<ContentVideoAnimation handleReady={handlePlay}>
+  const handleReplay = () => {
+    setShowPlayer(true);
+  }
+
+
+  return (<ContentVideoAnimation handlePlay={handlePlay} handlePause={handlePause} handleReplay={handleReplay}>
     <div className={'relative wrapper'}>
       <div
         className={cn('absolute bg-neutral w-full h-full overflow-hidden z-20 aspect-w-16 aspect-h-9', (showPlayer) ? 'opacity-100 display' : 'opacity-0 hidden')}>
