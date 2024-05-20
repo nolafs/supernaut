79cc323ec4b9ebf732c1c2b13211b1ef7afe69e7
Olaf Siebert <olaf@biffnewmedia.com>
2024-05-20 16:29:25 +0200

    feat: animation test

--- libs/shared/utils/src/lib/page-transition.ts
+++ libs/shared/utils/src/lib/page-transition.ts
@@ -5,24 +5,27 @@ let ANIM = false;
 export const animatePageIn = () => {
   const cover = gsap.utils.toArray('.bar');
   const wrapper = gsap.utils.selector('#page-transition');
-  const contentContainer = document.getElementById('content-main');
+  //const text = gsap.utils.selector('#section-name');
+  const text = document.getElementById('section-name');
   const tl = gsap.timeline({paused: true});
-  tl.set(wrapper, {opacity: 0})
+
+
   if(cover.length && !ANIM) {
     ANIM = true;
-    console.log('animatePageIn');
-    document.body.classList.add('is-transitioning');
-
+    //document.body.classList.add('is-transitioning');
     tl.set(wrapper, {opacity: 1})
-    tl.to(cover, {duration: 1, y: window.innerHeight,scaleY: .2, transformOrigin: "0 0", ease: 'power3.inOut',  stagger: {
+    tl.fromTo(text, {opacity: 1}, {opacity: 0, duration: 0.2, ease: 'power3.In', delay: 0.5})
+    tl.to(cover, {duration: 1, y: window.innerHeight,scaleY: .2, transformOrigin: "0 0",  ease: 'power3.inOut',  stagger: {
         amount: 0.5,
         from: 'end',
         ease: 'power2.inOut',
       },
       onComplete: () => {
         ANIM = false;
-        document.body.classList.remove('is-transitioning');}
+        //document.body.classList.remove('is-transitioning');
+        //}
       }
+      }, '-=0.5'
       )
     tl.set(wrapper, {opacity: 0})
     tl.play();
@@ -31,6 +34,7 @@ export const animatePageIn = () => {
 
 export const animatePageOut = (href: string, router: any) => {
   const cover = gsap.utils.toArray('.bar');
+  const text = document.getElementById('section-name');
   const wrapper = gsap.utils.selector('#page-transition');
   if (cover.length) {
     console.log('animatePageOut');
@@ -38,17 +42,18 @@ export const animatePageOut = (href: string, router: any) => {
     const tl = gsap.timeline({paused: true});
     tl.set(wrapper, {opacity: 1})
     tl.set(cover, {y:-window.innerHeight, scaleY: .2})
+
     tl.to(cover,{
       duration: 1, y: 0, scaleY: 1, transformOrigin: "0 0", ease: 'power3.inOut', stagger: {
         amount: 0.5,
         from: 'end',
         ease: 'power2.inOut',
-      }, onComplete: () => {
-        console.log('href', href)
-        router.push(href);
-        //document.body.classList.remove('is-transitioning');
       }
     })
+    tl.fromTo(text, {opacity: 0, y: 400}, {opacity: 1, y: 0, duration: 0.8, ease: 'power3.Out', delay: 0.2, onComplete: () => {
+      console.log('routing')
+      router.push(href);
+      }}, '-=1')
     tl.set(wrapper, {opacity: 0})
     tl.play();
 
